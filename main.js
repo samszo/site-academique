b37345b86f7818b87320a230f0f6ec6ae9cb1ca3
samszo <samszon@gmail.com>
2024-09-06 09:48:59 +0200

    navigation one page

--- main.js
+++ main.js
@@ -10,21 +10,28 @@ import {transcription} from './modules/transcription.js';
         //accordion = document.getElementById('accordionJDC'),
         //rectAccordion = accordion.getBoundingClientRect(),
         wait = new loader(),
-        cherche;
-        
-
-    //dimensionne les contenus
-    let rectFooter = d3.select('footer').node().getBoundingClientRect(),
-    rectHeader = d3.select('header').node().getBoundingClientRect(),
-    hMap = rectFooter.top-rectFooter.height-rectHeader.bottom;
-    d3.select('#contentMap').style('height',hMap+"px");
-    d3.select('#contentResources').style('height',hMap+"px");
-    let  rectMap = d3.select('#contentMap').node().getBoundingClientRect(),
-        wMap = rectMap.width,
         intervalId = window.setInterval(function(){
-        changePapillon();
-      }, 6000);
+            changePapillon();
+            aleaDia();
+        }, 6000),
+        confs;
+
+    d3.json("assets/data/confsInfos.js")
+    .then(data => {
+        confs = data;
+        aleaDia();
+    });                
+
     changePapillon();
+    chargeCitations();
+
+    function aleaDia(){
+        let conf = confs[d3.randomInt(0, confs.length-1)()],
+        numDia = d3.randomInt(0, conf.diapos.length-1)(),
+        url = conf.urlSlide+'?diapo='+numDia;        
+        d3.select("#aleaDiapo").style('display','block').attr('src',url);
+    }
+
 
     function changePapillon(){
         let url = "../ChaoticumPapillonae/CreaPapiDynaAnim.php?anim=0&larg=64&haut=64&id=svgPapi";
@@ -35,3 +42,37 @@ import {transcription} from './modules/transcription.js';
 
     }
     
+    function chargeCitations(){
+        let url = "../samszo/omk/s/cartoaffect/page/ajax?json=1&helper=citations";
+        d3.json(url).then(rs=>{
+            console.log(rs);
+            /*
+            <div class="carousel-item active">
+                <div class="container">
+                    <figure>
+                        <blockquote class="blockquote">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">
+                            Someone famous in <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+            */
+            let items = d3.select("#carouselWorksItems").selectAll('div').data(rs).enter().append('div')
+                .attr('class',(d,i)=>"carousel-item"+(i==0 ? " active" : ""))
+                .append('div').attr('class',"container")
+                .append('figure');
+            items.append('blockquote').attr('class',"blockquote").append('p').attr('class',"fw-light")
+                .html(d=>d.text.replace(/(<([^>]+)>)/gi, ""));
+            let ref = items.append('blockquote').attr('class',"blockquote-footer");
+            ref.selectAll('span').data(d=>{
+                return d.creators;
+            }).enter().append('span').attr('class','text-uppercase fw-bold me-1').html((d,i)=>{
+                return i>0 ? ', '+d.nom : d.nom;
+            });
+            ref.append('cite').html(d=>d.titre)
+        })
+    }
